dce3f5a75a9778abe334fc41150929d429bf292a
miguelmc <mcatedral24@gmail.com>
2023-11-18 04:50:54 -0600

    The conexion to databases was created

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express'
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import db from './config/db.js'
 //Creating app
 const app = express()
+// conexion to databases
+try{
+    await db.authenticate();
+    console.log("Conexion correct to databases");
+}catch(error){
+    console.log(error);
+}
 //Hability Pug
 app.set('view engine', 'pug')
 app.set('views', './views')
@@ -12,7 +20,7 @@ app.use('/auth', usuarioRoutes)
 //Define a port and start project
 const port = 3300
 app.listen(port,()=>{
-    console.log("Working");
+    console.log("The server is working in the port 3300");
 })
 
 // about the middeware: https://chat.openai.com/c/eedbfe55-2a93-49e7-93e6-6162fbb12e9f
