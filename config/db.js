1e01dd69c98a980cd058cb08c7fe37e88ea13004
miguelmc <mcatedral24@gmail.com>
2023-11-22 02:00:22 -0600

    The file .env was created

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv'
+dotenv.config({path:'.env'})
 
-const db = new Sequelize('distribuidoraLaFamilia', 'root', 'rebHaraya314',{
-    host:'localhost',
-    port:3306,
+const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASSWORD,{
+    host:process.env.BD_HOST,
+    port:process.env.BD_PORT,
     dialect:"mysql",
     define:{
         timestamps:true,
